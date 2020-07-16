57f66b6da2fb345171ce0e4e083e078752d024d8
Daniel Walsh <walshdan9163@gmail.com>
2020-07-15 23:22:52 -0400

    Change connection setup

--- boyd.js
+++ boyd.js
@@ -3,14 +3,14 @@ const ytdl = require('ytdl-core');
 const townRoad = function(msg){
     if(msg.content == '!boydTownRoad' && !msg.author.bot){
         if(msg.member.voiceChannel){
-            const connection = msg.member.voiceChannel.join();
-            const dispatcher = connection.playFile(ytdl('https://www.youtube.com/watch?v=7ysFgElQtjI&pbjreload=101', {filter:'audioonly'}));
-
-            dispatcher.on('finish', () => {
-                msg.reply("Hope you enjoyed!");
-            });
-
-            connection.disconnect();
+            msg.member.voiceChannel.join()
+                .then(connection => {
+                    connection.playFile(ytdl('https://www.youtube.com/watch?v=7ysFgElQtjI&pbjreload=101', {filter:'audioonly'}))
+                        .on('end', () => {
+                            voiceChannel.leave();
+                        })
+                })
+                .catch(console.log);
         }
         else{
             msg.reply('You need to join a voice channel first!');
