4757fc5b9a99ca2e24f9dd708b6c9be45c290ae9
Daniel Walsh <walshdan9163@gmail.com>
2020-07-16 09:08:55 -0400

    Use dispatcher

--- boyd.js
+++ boyd.js
@@ -5,10 +5,13 @@ const townRoad = function(msg){
         if(msg.member.voiceChannel){
             msg.member.voiceChannel.join()
                 .then(connection => {
-                    connection.playFile(ytdl('https://www.youtube.com/watch?v=7ysFgElQtjI&pbjreload=101', {filter:'audioonly'}))
-                        .on('end', () => {
-                            voiceChannel.leave();
-                        })
+                    const dispatcher = connection.playFile(ytdl('https://www.youtube.com/watch?v=7ysFgElQtjI&pbjreload=101', {filter:'audioonly'}))
+                    dispatcher.on('end', () => {
+                        voiceChannel.leave();
+                    });
+                    dispatcher.on('error', e => {
+                        console.log(e);
+                    });
                 })
                 .catch(console.log);
         }
