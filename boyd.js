c316372c1536d88a8f2d011ae6c20aa4c88306dd
Daniel Walsh <walshdan9163@gmail.com>
2020-07-15 23:13:30 -0400

    Changed to playFile

--- boyd.js
+++ boyd.js
@@ -4,7 +4,7 @@ const townRoad = function(msg){
     if(msg.content == '!boydTownRoad' && !msg.author.bot){
         if(msg.member.voiceChannel){
             const connection = msg.member.voiceChannel.join();
-            const dispatcher = connection.play(ytdl('https://www.youtube.com/watch?v=7ysFgElQtjI&pbjreload=101', {filter:'audioonly'}));
+            const dispatcher = connection.playFile(ytdl('https://www.youtube.com/watch?v=7ysFgElQtjI&pbjreload=101', {filter:'audioonly'}));
 
             dispatcher.on('finish', () => {
                 msg.reply("Hope you enjoyed!");
