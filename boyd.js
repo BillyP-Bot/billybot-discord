a0b97b6177e2276b8f5e20736766ddce83e211de
Daniel Walsh <walshdan9163@gmail.com>
2020-07-15 22:59:24 -0400

    Added music dispatcher

--- boyd.js
+++ boyd.js
@@ -1,7 +1,15 @@
+const ytdl = require('ytdl-core');
+
 const townRoad = function(msg){
     if(msg.content == '!boydTownRoad' && !msg.author.bot){
         if(msg.member.voiceChannel){
             const connection = msg.member.voiceChannel.join();
+            const dispatcher = connection.play(ytdl('https://www.youtube.com/watch?v=7ysFgElQtjI&pbjreload=101', {filter:'audioonly'}));
+
+            dispatcher.on('finish', () => {
+                msg.reply("Hope you enjoyed!");
+            });
+            
         }
         else{
             msg.reply('You need to join a voice channel first!');
