6154e4da236b718dd8a9cadbc358f699dbc53fb1
Daniel Walsh <walshdan9163@gmail.com>
2020-07-17 14:42:36 -0400

    Attempting to cache images and return random image from cache

--- dianne.js
+++ dianne.js
@@ -1,21 +1,28 @@
 const fetch = require('node-fetch');
 
 // Meme API: https://github.com/R3l3ntl3ss/Meme_Api
+var cachedImages = [];
 
 const fridayFunny = function(msg){
     if(msg.content == '!Dianne' || msg.content == '!dianne'  && !msg.author.bot){
-        var fridayFunny = []
-        fetch('https://www.reddit.com/r/boomershumor/hot.json?limit=1&over_18=false')
+        if(cachedImages.length = 0){
+            fetch('https://www.reddit.com/r/boomershumor/hot.json?limit=50&over_18=false')
             .then(response => response.json())
             .then(data =>  {
                 data.data.children.forEach(redditPost => {
                     if(redditPost.data.selftext == ""){
-                        fridayFunny.push(redditPost.data.url);
+                        cachedImages.push(redditPost.data.url);
+                        console.log('Populated cachedImages');
                     }
                 })
-
-                msg.reply("I just found this great meme on my Facebook feed!\n\n", {files: fridayFunnies});
             });
+        }
+        else{
+            console.log('cachedImages contains content');
+        }
+        var randomInt = getRandomIntInclusive(0, cachedImages.length)
+        var fridayFunny = cachedImages.splice(randomInt, 1);
+        msg.reply("I just found this great meme on my Facebook feed!\n\n", {files: fridayFunny});
     }
 }
 
@@ -34,5 +41,11 @@ const fridayFunnies = function(msg){
             });
     }
 }
+
+const getRandomIntInclusive = function(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
 
 module.exports = { fridayFunny, fridayFunnies };
