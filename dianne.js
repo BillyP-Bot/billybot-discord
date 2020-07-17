8a68781ec4e7d894099a16fe03ee8b62fbc363b7
Daniel Walsh <walshdan9163@gmail.com>
2020-07-17 11:32:19 -0400

    Reverted changes

--- dianne.js
+++ dianne.js
@@ -1,16 +1,13 @@
 const fetch = require('node-fetch');
 
+// Meme API: https://github.com/R3l3ntl3ss/Meme_Api
+
 const fridayFunny = function(msg){
     if(msg.content == '!fridayFunny' || msg.content == '!Dianne' || msg.content == '!dianne'  && !msg.author.bot){
         fetch('https://meme-api.herokuapp.com/gimme/boomershumor')
             .then(response => response.json())
             .then(data =>  {
-                if(!data.NSFW){
-                    msg.reply(data.url);
-                }
-                else{
-                    this.fridayFunny(msg);
-                }
+                msg.reply(data.url);
             });
     }
 }
