60cf8cc799cc098e1ebbb6a8754b582b7ec5db39
Daniel Walsh <walshdan9163@gmail.com>
2020-07-16 21:09:10 -0400

    Used playStream

--- boyd.js
+++ boyd.js
@@ -5,7 +5,7 @@ const townRoad = function(msg){
         if(msg.member.voiceChannel){
             msg.member.voiceChannel.join()
                 .then(connection => {
-                    const dispatcher = connection.playFile(ytdl('https://www.youtube.com/watch?v=7ysFgElQtjI&pbjreload=101', {filter:'audioonly'}));
+                    const dispatcher = connection.playStream(ytdl('https://www.youtube.com/watch?v=7ysFgElQtjI&pbjreload=101', {filter:'audioonly'}));
                     dispatcher.on('end', () => {
                         voiceChannel.leave();
                     });
