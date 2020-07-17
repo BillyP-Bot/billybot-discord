b6ae4c95f0f0d9514a4fd13e6f9fb40a728cbd26
Daniel Walsh <walshdan9163@gmail.com>
2020-07-17 11:02:18 -0400

    NSFW filter and added more commands

--- dianne.js
+++ dianne.js
@@ -1,11 +1,16 @@
 const fetch = require('node-fetch');
 
 const fridayFunny = function(msg){
-    if(msg.content == '!fridayFunny' && !msg.author.bot){
+    if(msg.content == '!fridayFunny' || msg.content == '!Dianne' || msg.content == '!dianne'  && !msg.author.bot){
         fetch('https://meme-api.herokuapp.com/gimme/boomershumor')
             .then(response => response.json())
             .then(data =>  {
-                msg.reply(data.url);
+                if(!data.NSFW){
+                    msg.reply(data.url);
+                }
+                else{
+                    fridayFunny(msg);
+                }
             });
     }
 }
