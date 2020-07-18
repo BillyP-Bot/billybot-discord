a200e3a4d9d48ad01f5cee320d024f3654b3641d
Daniel Walsh <walshdan9163@gmail.com>
2020-07-18 16:24:57 -0400

    Started goodFriday func

--- anthony.js
+++ anthony.js
@@ -3,11 +3,11 @@ const bot = require('./bot')
 
 var kanyeVideoIDs = [];
 
-const goodFriday = function(){
+const goodFriday = function(msg){
     if(kanyeVideoIDs.length == 0){
         // Request youtube API to populate videoIDs list
-        var general = bot.channels['general'];
-        bot.client.getMaxListeners(general).send('testing good friday func');
+        msg.channel.send('testing good friday func')
+            .catch(console.error);
     }
     else{
         // Post random video from list
@@ -16,12 +16,16 @@ const goodFriday = function(){
         var videoID = kanyeVideoIDs.splice(randomInt, 1);
         console.log('Kanye videos list length: ' + kanyeVideoIDs.length);
 
-        var general = bot.channels['general'];
-        bot.client.getMaxListeners(general).send('testing good friday func');
+        msg.channel.send('testing good friday func')
+            .catch(console.error);
+            
     }
 }
+
+module.exports = { goodFriday };
 
 // kanyewest channel id: UCs6eXM7s8Vl5WcECcRHc2qQ
 
 // kanyeVevo channel ID: UChpJbg7zMbi5jx9Gdjaxa9g
+
 // kanye west videos playlist ID: PLdYwhvDpx0FLEfzLe3BVZip4V4kAF1g1H
