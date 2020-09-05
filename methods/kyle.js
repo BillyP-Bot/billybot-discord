7eeff2a16fb931e395a5e252db4958ca82dd3168
Daniel Walsh <walshdan9163@gmail.com>
2020-09-05 15:42:54 -0400

    Fetch data from API

--- methods/kyle.js
+++ methods/kyle.js
@@ -1,66 +1,13 @@
 const Discord = require('discord.js');
+const fetch = require('node-fetch');
 
 const kylesId = '637446755897835556';
 const workStuffChannel = '689463821869383690';
 const billyMad = '694721037006405742';
 
-const drinkPrefix = '!kyle';
+const drinkPrefix = '!drink';
 const ingredientPrefix = '!ingredients';
 const categoryPrefix = '!category';
-
-const exampleDrink = {
-    drink: '1-900-FUK-MEUP',
-    drinkId: '420',
-    alcoholic: 'Alchoholic',
-    category: 'Shot',
-    glass: 'Old-fashioned glass',
-    IBA: null,
-    ingredients: ['Absolut Kurant', 'Grand Marnier', 'Chambord raspberry liqueur', 'Midori melon liqueur', 'Malibu rum', 'Amaretto', 'Cranberry juice', 'Pineapple juice'],
-    measures: ['1/2 oz', '1/4 oz', '1/4 oz', '1/4 oz', '1/4 oz', '1/4 oz', '1/2 oz', '1/4 oz'],
-    instructions: 'Shake ingredients in a mixing tin filled with ice cubes. Strain into a rocks glass.',
-    image: 'http://www.thecocktaildb.com/images/media/drink/uxywyw1468877224.jpg'
-}
-
-const listOfDrinks = {
-    drinks: [
-        {
-            drink: '1-900-FUK-MEUP',
-            drinkId: '420',
-            alcoholic: 'Alchoholic',
-            category: 'Shot',
-            glass: 'Old-fashioned glass',
-            IBA: null,
-            ingredients: ['Absolut Kurant', 'Grand Marnier', 'Chambord raspberry liqueur', 'Midori melon liqueur', 'Malibu rum', 'Amaretto', 'Cranberry juice', 'Pineapple juice'],
-            measures: ['1/2 oz', '1/4 oz', '1/4 oz', '1/4 oz', '1/4 oz', '1/4 oz', '1/2 oz', '1/4 oz'],
-            instructions: 'Shake ingredients in a mixing tin filled with ice cubes. Strain into a rocks glass.',
-            image: 'http://www.thecocktaildb.com/images/media/drink/uxywyw1468877224.jpg'
-        },
-        {
-            drink: '1-900-FUK-MEUP',
-            drinkId: '420',
-            alcoholic: 'Alchoholic',
-            category: 'Shot',
-            glass: 'Old-fashioned glass',
-            IBA: null,
-            ingredients: ['Absolut Kurant', 'Grand Marnier', 'Chambord raspberry liqueur', 'Midori melon liqueur', 'Malibu rum', 'Amaretto', 'Cranberry juice', 'Pineapple juice'],
-            measures: ['1/2 oz', '1/4 oz', '1/4 oz', '1/4 oz', '1/4 oz', '1/4 oz', '1/2 oz', '1/4 oz'],
-            instructions: 'Shake ingredients in a mixing tin filled with ice cubes. Strain into a rocks glass.',
-            image: 'http://www.thecocktaildb.com/images/media/drink/uxywyw1468877224.jpg'
-        },
-        {
-            drink: '1-900-FUK-MEUP',
-            drinkId: '420',
-            alcoholic: 'Alchoholic',
-            category: 'Shot',
-            glass: 'Old-fashioned glass',
-            IBA: null,
-            ingredients: ['Absolut Kurant', 'Grand Marnier', 'Chambord raspberry liqueur', 'Midori melon liqueur', 'Malibu rum', 'Amaretto', 'Cranberry juice', 'Pineapple juice'],
-            measures: ['1/2 oz', '1/4 oz', '1/4 oz', '1/4 oz', '1/4 oz', '1/4 oz', '1/2 oz', '1/4 oz'],
-            instructions: 'Shake ingredients in a mixing tin filled with ice cubes. Strain into a rocks glass.',
-            image: 'http://www.thecocktaildb.com/images/media/drink/uxywyw1468877224.jpg'
-        }
-    ]
-}
 
 const kyleNoWorking = function(msg){
     if(msg.author.id == kylesId && msg.channel == workStuffChannel){
@@ -72,14 +19,39 @@ const kyleNoWorking = function(msg){
 
 const getKyleCommand = function(msg){
     if (msg.content.startsWith(drinkPrefix) && !msg.author.bot){
-        const args = msg.content.slice(drinkPrefix.length).trim().split(' ');
-        const command = args.shift().toLowerCase();
-        msg.reply(getDrinkByName(command));
+        const command = msg.content.replace(drinkPrefix + " ", "").replace(" ", "%20");
+        getDrinkByName(command)
+            .then(returnedDrink => {
+                if(returnedDrink.length > 0){
+                    const drink = returnedDrink[0];
+                    const drinkEmbed = new Discord.MessageEmbed()
+                    .setColor('#0099ff')
+                    .addField('Glass:', drink.glass)
+                    .setTitle('Drink Name: ' + drink.drink)
+                    .setDescription(drink.instructions)
+                    .setImage(drink.drink_thumb)
+                    for(var i = 0; i < drink.ingredients.length; i++){
+                        drinkEmbed.addField(drink.ingredients[i] + ':', drink.measurement[i])
+                    }
+                    msg.reply(drinkEmbed);
+                }
+                else{
+                    msg.reply("Sorry, we don't have instructions for that one!");
+                }
+            })
     }
     else if(msg.content.startsWith(ingredientPrefix) && !msg.author.bot){
-        const args = msg.content.slice(ingredientPrefix.length).trim().split(' ');
-        const command = args.shift().toLowerCase();
-        msg.reply(getDrinksByIngredients(command));
+        const command = msg.content.replace(ingredientPrefix + " ", "").replace(" ", "%20");
+        getDrinksByIngredients(command)
+            .then(returnedDrinks => {
+                var messageReply = "Here's the list of drinks with the provided ingredient(s)!\n"
+                for(var i = 0; i < returnedDrinks.length; i++){
+                    messageReply += returnedDrinks[i].drink + '\n';
+                }
+                messageReply += '\nRequest ingredients and instructions by typing !kyle followed by the desired drink name!';
+                msg.reply(messageReply)
+            })
+
     }
     else if(msg.content.startsWith(categoryPrefix) && !msg.author.bot){
         const args = msg.content.slice(categoryPrefix.length).trim().split(' ');
@@ -88,45 +60,34 @@ const getKyleCommand = function(msg){
     }
 }
 
-const getDrinkByName = function(drink){
-    // fetch('ourapilink/drink/name')
-    //     .then(response => response.json())
-    //     .then(data =>  {
-    //         return data;
-    //     });
-    const result = exampleDrink;
-    const exampleEmbed = new Discord.MessageEmbed()
-	.setColor('#0099ff')
-	.setTitle('Drink Name: ' + result.drink)
-	.setDescription(result.instructions)
-    .setImage(result.image)
-    for(var i = 0; i < result.ingredients.length; i++){
-        exampleEmbed.addField(result.ingredients[i], result.measures[i], true)
-    }
-    return exampleEmbed;
+const getDrinkByName = async function(drink){
+    const returnedDrink = fetch('https://cocktails-heroku.herokuapp.com/cocktails/drink/?drink=' + drink)
+        .then(response => response.json())
+        .then(data => {
+            return data;
+        })
+
+    return returnedDrink;
 }
 
 const getDrinksByIngredients = function(ingredients){
-    // fetch('ourapilink/ingredients/name')
-    //     .then(response => response.json())
-    //     .then(data =>  {
-    //         return data;
-    //     });
-    const resultDrinks = listOfDrinks;
-    var messageReply = "Here's the list of drinks with the provided ingredient(s)!\n"
-    for(var i = 0; i < resultDrinks.drinks.length; i++){
-        messageReply += resultDrinks.drinks[i].drink + '\n';
-    }
-    messageReply += '\nRequest ingredients and instructions by typing !kyle followed by the desired drink name!';
-    return messageReply;
+    const returnedDrinks = fetch('https://cocktails-heroku.herokuapp.com/cocktails/ingredient/?ingredient=' + ingredients)
+        .then(response => response.json())
+        .then(data => {
+            return data;
+        })
+
+    return returnedDrinks;
+
+    
 }
 
 const getDrinksByCategory = function(category){
-    // fetch('ourapilink/category/name')
-    //     .then(response => response.json())
-    //     .then(data =>  {
-    //         return data;
-    //     });
+    fetch('ourapilink/category/name')
+        .then(response => response.json())
+        .then(data =>  {
+            return data;
+        });
     const resultDrinks = listOfDrinks;
     var messageReply = "Here's the list of drinks with the provided category!\n"
     for(var i = 0; i < resultDrinks.drinks.length; i++){
