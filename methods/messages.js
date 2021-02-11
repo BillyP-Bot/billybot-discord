06f618aee9fd0d4ccea348f40e4020215debc4a9
JudeBoy <judegiordano@gmail.com>
2021-02-11 11:27:25 -0500

    Post to Rolling Log Db

--- methods/messages.js
+++ methods/messages.js
@@ -1,21 +1,45 @@
 const Discord = require('discord.js');
+const fetch = require('node-fetch');
 
 const adminMsgPrefix = '!adminMsg';
 
+const postLog = (log, issuer) => {
+	return new Promise((resolve, reject) => {
+		const bod = {
+			log: log,
+			issuer: issuer
+		};
+		fetch('https://btbackend.herokuapp.com/api/logs/newlog', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify(bod)
+		}).then(r => r.json())
+			.then(data => {
+				return resolve(data);
+			}).catch(e => {
+				reject(e);
+			});
+	});
+};
+
 const adminMsg = (msg, client) => {
-	if (msg.content.startsWith(adminMsgPrefix) && msg.channel.type === 'dm'){
+	if (msg.content.startsWith(adminMsgPrefix) && msg.channel.type === 'dm') {
 		const adminText = msg.content.replace(adminMsgPrefix, '').trim();
 		const generalChannel = client.channels.cache.find(TextChannel => TextChannel.name === 'general');
+
+		postLog(adminText, msg.author.username).then(r => console.log(r));
+
 		const card = new Discord.MessageEmbed()
 			.setColor('#1bb0a2')
 			.setTitle('Admin Update')
-			.addField(`Update From ${msg.author.username}`, adminText);
-		
+			.addField(`Update From ${msg.author.username}`, adminText)
+			.addField('Rolling Log', 'See all changelogs [here](https://btbackend.herokuapp.com/api/logs)!');
+
 		return generalChannel.send(card)
 			.then(console.log(`Sent Admin message: ${adminText}`))
-			.catch(function(e){
-				console.error(e);}
-			);
+			.catch((e) => {
+				console.error(e);
+			});
 	}
 };
 
