fefad6b08b7a289d194cce94d3868a6f11d2cfb8
Daniel Walsh <djwalsh@umass.edu>
2021-06-04 15:08:24 -0400

    Literally wrote this on my iPhone

--- src/methods/joe.ts
+++ src/methods/joe.ts
@@ -1,1 +1,6 @@
 
+import { Message } from "discord.js";
+
+export const joe = (Message msg) => {
+	msg.reply(https://cdn.discordapp.com/attachments/689463685571149933/847826319370092544/friday.mp4);
+};
