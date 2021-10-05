628e7bc9f3c9acaf862d025e232fdb3ed92ec1c5
flogan <flogan.logan808@gmail.com>
2021-10-05 18:16:18 -0400

    currency hot fixes 🥵

--- src/methods/currency.ts
+++ src/methods/currency.ts
@@ -126,22 +126,19 @@ export default class Currency {
 		}
 	}
 
-	public static async BuckReact(react: MessageReaction, userId: string, add: boolean): Promise<void> {
+	public static async BuckReact(react: MessageReaction, userId: string): Promise<void> {
 		try {
 			const guildId: string = react.message.guild.id;
 			const authorId: string = react.message.author.id;
+			//check configured
 			const author$: number = await User.GetBucks(authorId, react.message.guild.id);
 			const user$: number = await User.GetBucks(userId, react.message.guild.id);
-			if (author$ && user$){ //Update bucks if configured and have money otherwise do nothing
-				if (user$ > 0 && add) {
-					User.UpdateBucks(authorId, guildId, 1, true);
-					User.UpdateBucks(userId, guildId, -1, true);
-				} else {
-					User.UpdateBucks(authorId, guildId, -1, true);
-					User.UpdateBucks(userId, guildId, 1, true);
-				}
+			if (user$ > 0) {
+				User.UpdateBucks(authorId, guildId, 1, true);
+				User.UpdateBucks(userId, guildId, -1, true);
 			}
-		} catch (error) {
+		}
+		catch (error) {
 			if (error === "user not found")
 				logger.warn(error);
 			else 
@@ -157,6 +154,14 @@ export default class Currency {
 			const userBucks: number = await User.GetBucks(msg.author.id, msg.guild.id);
 
 			if (username) {
+				if (username === msg.author.username){
+						buckEmbed.setColor(Colors.red);
+						buckEmbed.setTitle("Error");
+						buckEmbed.setDescription(`You cannot pay yourself, ${username}!`);
+						
+						msg.reply(buckEmbed);
+						return;
+				}
 				const found: Discord.GuildMember = msg.guild.members.cache.find(a => a.user.username.toUpperCase() === username.toUpperCase().trim());
 					if (!found) {
 						buckEmbed.setColor(Colors.red);
@@ -164,6 +169,7 @@ export default class Currency {
 						buckEmbed.setDescription(`Could not find ${username} in this server.`);
 						
 						msg.reply(buckEmbed);
+						return;
 					}
 					else {
 						if (payAmount) {
@@ -172,6 +178,7 @@ export default class Currency {
 								buckEmbed.setColor(Colors.red).setTitle("Error");
 								buckEmbed.setDescription(`You do not have ${payAmount} BillyBucks!`);
 								msg.reply(buckEmbed);
+								return;
 							}
 							const updated: boolean = await User.UpdateBucks(user.id, msg.guild.id, +payAmount, true);
 							const updated2: boolean = await User.UpdateBucks(msg.author.id, msg.guild.id, -payAmount, true);
