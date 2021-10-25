29fdc8c10414c9ec1b75d48662cce0793150b5fe
JudeBoy <judegiordano@gmail.com>
2021-10-25 13:26:13 -0400

    Strict REST Class For Stocks / Remove 3rd Party pkg

--- src/services/rest.ts
+++ src/services/rest.ts
@@ -19,5 +19,23 @@ export class Rest {
 			throw new Error(error);
 		}
 	}
+}
 
+export class StockApi {
+
+	private static readonly base = "https://finance.yahoo.com/quote";
+
+	private static readonly client = axios.create({
+		baseURL: StockApi.base
+	});
+
+	public static async GetPrice(ticker: string): Promise<number> {
+		const symbol = ticker.toUpperCase().trim();
+		const { data } = await StockApi.client.get(symbol);
+		const price = data.split(`"${symbol}":{"sourceInterval"`)[1]
+			.split("regularMarketPrice")[1]
+			.split("fmt\":\"")[1]
+			.split("\"")[0];
+		return parseFloat(price);
+	}
 }
