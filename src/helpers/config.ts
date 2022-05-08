61eb292748c7288b8f3badb6ef2883ec411d6bc5
Jude Giordano <44903893+judegiordano@users.noreply.github.com>
2022-05-08 16:28:43 -0400

    Ad Roulette Spin Back (#183)
    * Ad Roulette Spin Back
    
    * Delete Roulette File

--- src/helpers/config.ts
+++ src/helpers/config.ts
@@ -1,15 +1,12 @@
 import "dotenv/config";
-import path from "path";
 
 export const config = {
 	SERVER_ID: "689463685566955566",
-	IS_COMPILED: path.extname(__filename).includes("js"),
 	IS_PROD: (process.env.NODE_ENV == "production" ? true : false),
 	BOT_TOKEN: process.env.BOT_TOKEN || undefined,
 	GOOGLE_API_KEY: process.env.GOOGLE_API_KEY || undefined,
 	GOOGLE_CX_KEY: process.env.GOOGLE_CX_KEY || undefined,
 	YOUTUBE_API_KEY: process.env.YOUTUBE_API_KEY || undefined,
-
 	BILLY_BACKEND: process.env.BILLY_BACKEND,
 	BILLY_BACKEND_TOKEN: process.env.BILLY_BACKEND_TOKEN,
 };
