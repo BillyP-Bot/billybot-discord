88a5569280c78769a0af4ff30c7f5c33f25a837a
Chris Leveille <cdleveille@gmail.com>
2022-05-16 21:49:41 -0400

    ignore bot-testing channel in prod

--- src/Index.ts
+++ src/Index.ts
@@ -573,6 +573,7 @@ async function blackJackStand(msg: Message) {
 client.on("message", async (msg: Message) => {
 	try {
 		if (msg.channel.type === "dm") return;
+		if (msg.channel.id === "975795297257136189" && config.IS_PROD) return;
 		if (msg.author.bot) return;
 		switch (true) {
 			case /.*(!help).*/gmi.test(msg.content):
