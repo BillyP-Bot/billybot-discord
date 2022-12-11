a1813d2eb9dc2a7401e54db7283c32bd11ed692d
Chris Leveille <cdleveille@gmail.com>
2022-12-11 18:20:39 -0500

    allow !album to be run with other user as target

--- src/commands/album.ts
+++ src/commands/album.ts
@@ -3,7 +3,7 @@ import type { Message } from "discord.js";
 import type { ICommand } from "../types";
 import type { IOpenAiImage } from "btbot-types";
 
-import { Api } from "../helpers";
+import { Api, getServerDisplayName } from "../helpers";
 import { sendPaginatedImageList } from "../helpers/embed";
 
 export const albumCommand: ICommand = {
@@ -11,12 +11,14 @@ export const albumCommand: ICommand = {
 	command: "!album",
 	description: "View an album of all of your previously generated images.",
 	handler: async (msg: Message) => {
-		const res = await Api.get<IOpenAiImage[]>(
-			`images?server_id=${msg.guild.id}&user_id=${msg.author.id}`
-		);
+		const { name, id } = getServerDisplayName(msg);
+		const isSelf = msg.author.id === id;
+		const res = await Api.get<IOpenAiImage[]>(`images?server_id=${msg.guild.id}&user_id=${id}`);
 		if (!res || res.length === 0) {
-			msg.reply(
-				"You have not generated any images yet! Run `!image [prompt]` to generate an image."
+			msg.channel.send(
+				`${isSelf ? "You have" : `${name} has`} not generated any images yet! ${
+					isSelf ? "Run `!image [prompt]` to generate an image." : ""
+				}`
 			);
 			return;
 		}
