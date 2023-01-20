5002ba53a36b1cc0a251d3107ccc4caa75b709b5
Forrest Logan <flogan@umass.edu>
2023-01-20 09:15:04 -0500

    Fl/close bets (#222)
    * initial commit close bets
    
    * Add listing bets to close command

---
+++ src/commands/close-bet.ts
@@ -0,0 +1,29 @@
+import type { Message } from "discord.js";
+
+import type { ICommand, BetAggregate } from "../types";
+import { Api, Embed, getServerDisplayName, buildCurrentBetsMessage } from "../helpers";
+import { IParticipant } from "btbot-types";
+
+export const closeBetCommand: ICommand = {
+	prefix: /.*!closebet .*/gim,
+	command: "!closebet",
+	description: "Close Betting on current mayoral challenge. Usage: `!closebet`",
+	handler: async (msg: Message) => {
+		const server_id = msg.guild.id;
+		const result = await Api.put<{
+			server_id: string;
+			participants: IParticipant[];
+			bets_aggregate: BetAggregate;
+		}>("/challenges/close", { server_id, author_id: msg.author.id });
+		const usernames = result.participants.map(({ user_id }) => {
+			const { name } = getServerDisplayName(msg, user_id);
+			return name;
+		});
+		const betsMessage = buildCurrentBetsMessage(msg, result.bets_aggregate);
+		const embed = Embed.success(
+			`Closed betting on current challenge between ${usernames[0]} and ${usernames[1]}\n\n${betsMessage}`
+		);
+		msg.channel.send(embed);
+		return;
+	}
+};
