d9de7579a0652f5ab21d60b35daf3548c472e618
jcarlsoniv <jcarlsoniv@gmail.com>
2023-02-15 17:13:36 -0500

    updating "fun fact" to "fun factoid" to mirror backend change

--- src/commands/fact-check.ts
+++ src/commands/fact-check.ts
@@ -7,12 +7,12 @@ import type { ICommand } from "../types";
 export const factCheckCommand: ICommand = {
 	prefix: /.*!factcheck.*/gim,
 	command: "!factcheck",
-	description: "Check if the latest Fun Fact of the Day is true or not.",
+	description: "Check if the latest Fun Factoid of the Day is true or not.",
 	handler: async (msg: Message) => {
 		const { fact } = await Api.get<IFunFact>("facts/latest");
-		if (!fact) throw "Error retrieving latest fun fact!";
+		if (!fact) throw "Error retrieving latest fun factoid!";
 		const prompt = `Is the following fact true? ${fact}`;
-		await msg.channel.send(`Fact-checking the latest Fun Fact of the Day...\n> *${fact}*`);
+		await msg.channel.send(`Fact-checking the latest Fun Factoid of the Day...\n> *${fact}*`);
 		const res = await Api.post<{ output: string }>("completions", {
 			prompt,
 			user_id: msg.author.id,
