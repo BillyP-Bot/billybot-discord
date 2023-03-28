2830707c511ee1a112aa1c5fddd22dd797b91ff6
flogan <flogan.logan808@gmail.com>
2023-03-28 16:54:55 -0400

    bump to node 18 clean up embed.ts

--- src/helpers/embed.ts
+++ src/helpers/embed.ts
@@ -25,11 +25,6 @@ export class Embed {
 }
 
 export const sendPaginatedCommandList = async (commands: ICommand[], msg: Message) => {
-	console.log(
-		commands.map((c) => {
-			return { name: c.command, value: c.description };
-		})
-	);
 	const pagEmbed = new PaginatedEmbed({
 		itemsPerPage: 10,
 		paginationType: "field",
@@ -44,38 +39,7 @@ export const sendPaginatedCommandList = async (commands: ICommand[], msg: Messag
 		);
 
 	await pagEmbed.send({ options: { channel: msg.channel as TextChannel } });
-
-	// const embeds = constructCommandsEmbedArray(commands);
-	// await new Embeds()
-	// 	.setArray(embeds.map((embed) => embed.data))
-	// 	.setAuthorizedUsers(msg.author.id)
-	// 	.setChannel(msg.channel as TextChannel)
-	// 	.setTimeout(60000)
-	// 	.build();
 };
-
-// const constructCommandsEmbedArray = (commands: ICommand[]) => {
-// 	const commandsPerPage = 10;
-// 	const pageCount = Math.ceil(commands.length / commandsPerPage);
-// 	return commands.reduce((acc, { command, description }) => {
-// 		if (!command) return acc;
-// 		const lastEmbed = acc[acc.length - 1];
-// 		const commandsAdded =
-// 			Math.max(acc.length - 1, 0) * commandsPerPage + lastEmbed?.data.fields.length || 0;
-// 		if (commandsAdded % commandsPerPage === 0) {
-// 			const embed = new EmbedBuilder();
-// 			const pageNumber = Math.ceil(commandsAdded / commandsPerPage) + 1;
-// 			embed
-// 				.setColor(Colors.green)
-// 				.setTitle(`Commands (${pageNumber}/${pageCount})`)
-// 				.addFields({ name: command, value: description });
-// 			acc.push(embed);
-// 			return acc;
-// 		}
-// 		lastEmbed.addFields({ name: command, value: description });
-// 		return acc;
-// 	}, [] as EmbedBuilder[]);
-// };
 
 export const sendPaginatedImageList = async (
 	images: IOpenAiImage[],
