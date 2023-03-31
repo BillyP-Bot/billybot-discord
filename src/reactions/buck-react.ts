2f776066cd77d86058ed5b84254b20379831ddb8
flogan <flogan.logan808@gmail.com>
2023-03-30 20:40:53 -0400

    Suppress irrelevant emote errors to console

--- src/reactions/buck-react.ts
+++ src/reactions/buck-react.ts
@@ -3,10 +3,14 @@ import type { MessageReaction } from "discord.js";
 import { Api } from "../helpers";
 
 export async function buckReact(react: MessageReaction, sender_id: string) {
-	return Api.post("bucks/pay", {
-		server_id: react.message.guild.id,
-		amount: 1,
-		recipient_id: react.message.author.id,
-		sender_id
-	});
+	try {
+		return await Api.post("bucks/pay", {
+			server_id: react.message.guild.id,
+			amount: 1,
+			recipient_id: react.message.author.id,
+			sender_id
+		});
+	} catch (error) {
+		console.log(error);
+	}
 }
