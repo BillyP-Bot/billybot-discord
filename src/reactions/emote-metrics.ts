046e8f97dd11de4c77a09c895d1731ae8fe0a857
Forrest Logan <flogan@umass.edu>
2023-03-31 11:13:47 -0400

    Suppress irrelevant emote errors to console (#231)

--- src/reactions/emote-metrics.ts
+++ src/reactions/emote-metrics.ts
@@ -17,7 +17,7 @@ export async function updateEmoteMetrics(react: MessageReaction, sender_id: stri
 				engagement: { reactions_received: 1 }
 			}
 		];
-		return Api.put("metrics/engagement", body);
+		return await Api.put("metrics/engagement", body);
 	} catch (error) {
 		console.log({ error });
 	}
