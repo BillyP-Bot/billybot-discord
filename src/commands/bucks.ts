1fc5ea1394202aad2e7483bea9ac4dd5d85fea68
Chris Leveille <cdleveille@gmail.com>
2023-04-06 15:02:46 -0400

    /bucks

--- src/commands/bucks.ts
+++ src/commands/bucks.ts
@@ -1,19 +1,54 @@
-import type { Message } from "discord.js";
+import { ApplicationCommandOptionType, ChatInputCommandInteraction, Message } from "discord.js";
+
+import {
+	Api,
+	Embed,
+	getInteractionOptionValue,
+	getServerDisplayName,
+	getUserIdFromAtMention,
+	getUserIdFromUsername,
+	isAtMention
+} from "../helpers";
 
 import type { IUser } from "btbot-types";
 import type { ICommand } from "../types";
-import { Api, Embed, getServerDisplayName } from "../helpers";
-
 export const bucksCommand: ICommand = {
 	prefix: /.*!bucks.*/gim,
 	command: "!bucks",
 	description: "Get your current balance of BillyBucks, (Optional `!bucks [username/@user]`) ",
 	handler: async (msg: Message) => {
-		const { name, id } = getServerDisplayName(msg);
-		const data = await Api.get<IUser>(`users?user_id=${id}&server_id=${msg.guild.id}`);
-		const user = data;
-		const embed = Embed.success(`${name} has ${user.billy_bucks} BillyBucks!`);
+		const { id } = getServerDisplayName(msg);
+		const { billy_bucks } = await getUser(id, msg.guild.id);
+		const embed = Embed.success(`<@${id}> has ${billy_bucks} BillyBucks!`);
 		msg.channel.send({ embeds: [embed] });
 		return;
+	},
+	slash: {
+		name: "bucks",
+		description: "Get your or another user's current balance of BillyBucks",
+		options: [
+			{
+				name: "user",
+				description: "The @mention or username of the user (runs on self if omitted)",
+				type: ApplicationCommandOptionType.String
+			}
+		],
+		handler: async (int: ChatInputCommandInteraction) => {
+			let userId = int.user.id;
+			const userOptionValue = getInteractionOptionValue<string>("user", int);
+			if (userOptionValue) {
+				if (isAtMention(userOptionValue)) {
+					userId = getUserIdFromAtMention(userOptionValue);
+				} else {
+					userId = getUserIdFromUsername(userOptionValue, int.guild);
+				}
+			}
+			const { billy_bucks } = await getUser(userId, int.guild.id);
+			const embed = Embed.success(`<@${userId}> has ${billy_bucks} BillyBucks!`);
+			int.reply({ embeds: [embed] });
+		}
 	}
 };
+
+const getUser = async (user_id: string, server_id: string) =>
+	Api.get<IUser>(`users?user_id=${user_id}&server_id=${server_id}`);
