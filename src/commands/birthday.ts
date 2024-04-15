2c0fef0f930f0be26892cd5145d9132ccd85840f
Chris Leveille <cdleveille@gmail.com>
2024-04-15 00:45:02 -0400

    fix formatting

--- src/commands/birthday.ts
+++ src/commands/birthday.ts
@@ -58,7 +58,7 @@ const birthday = async (user: IUser, birthdayToValidate: string) => {
 
 const isValidDate = (month: number, day: number) => {
 	const daysInMonth = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-    if (month < 1 || month > 12) return false;
-    if (day < 1 || day > daysInMonth[month - 1]) return false;
-    return true;
+	if (month < 1 || month > 12) return false;
+	if (day < 1 || day > daysInMonth[month - 1]) return false;
+	return true;
 };
