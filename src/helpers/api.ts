c262f068393597c66b9cad21823a91ba26e36564
Chris Leveille <cdleveille@gmail.com>
2024-05-20 23:35:45 -0400

    use native fetch api for requests to backend instead of apisauce

--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -1,41 +1,28 @@
-import { create } from "apisauce";
 import { REST } from "discord.js";
 
 import { ApiResponse, UserLookup } from "@types";
 
 import { config } from "./config";
 
-export class Api {
-	public static readonly client = create({
-		baseURL: config.BILLY_BACKEND,
+const request = async <T>(url: string, method: string, body?: unknown) => {
+	const res = await fetch(`${config.BILLY_BACKEND}/${url}`, {
+		method,
 		headers: {
 			Authorization: `Bearer ${config.BILLY_BACKEND_TOKEN}`,
-			"x-api-timestamp": null
-		}
+			"Content-Type": "application/json",
+			"x-api-timestamp": Date.now().toString()
+		},
+		body: JSON.stringify(body)
 	});
+	const data = (await res.json()) as ApiResponse & T;
+	if (!res.ok) throw data?.error ?? "internal server error";
+	return data as T & UserLookup;
+};
 
-	public static async get<T>(url: string) {
-		const { data, ok } = await Api.client.get<ApiResponse & T>(url);
-		if (!ok) throw data.error ?? "internal server error";
-		return data as T & UserLookup;
-	}
-
-	public static async post<T>(url: string, body?: unknown) {
-		const { data, ok } = await Api.client.post<ApiResponse & T>(url, body);
-		if (!ok) throw data.error ?? "internal server error";
-		return data as T & UserLookup;
-	}
-
-	public static async put<T>(url: string, body?: unknown) {
-		const { data, ok } = await Api.client.put<ApiResponse & T>(url, body);
-		if (!ok) throw data.error ?? "internal server error";
-		return data as T & UserLookup;
-	}
-}
-
-Api.client.axiosInstance.interceptors.request.use((config) => {
-	config.headers["x-api-timestamp"] = new Date().getTime();
-	return config;
-});
+export const Api = {
+	get: async <T>(url: string) => request<T>(url, "GET"),
+	post: async <T>(url: string, body?: unknown) => request<T>(url, "POST", body),
+	put: async <T>(url: string, body?: unknown) => request<T>(url, "PUT", body)
+};
 
 export const DiscordApi = new REST({ version: "10" }).setToken(config.BOT_TOKEN);
